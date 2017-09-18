5c293e93a404e46ba7011ac96d54b5f55dc2d18e
Davi Morales <davimmorales@gmail.com>
2017-09-18 10:58:46 -0300

    Updated so printing can work fine

--- Galetron/dataOutput/dataOutput.v
+++ Galetron/dataOutput/dataOutput.v
@@ -2,19 +2,20 @@ module dataOutput (dataC, address, IO_RAMwrite, clock, IO_RAMOutput);
 	 input IO_RAMwrite, clock;
     input [31:0] dataC;
     input [9:0] address;
-    output [31:0] IO_RAMOutput;
+    output reg [31:0] IO_RAMOutput;
 
 
     integer firstClock = 0;
 
-    reg [31:0] ramOut[20:0];
-    always @ ( posedge clock) begin
-
+    //reg [31:0] ramOut[20:0];
+    //always @ ( posedge clock) begin
+      always @ (*) begin
       if (IO_RAMwrite) begin
-        ramOut[address] = dataC;
+        //ramOut[address] = dataC;
+		  IO_RAMOutput = dataC;
       end
     end
 
-    assign IO_RAMOutput = ramOut[address];
+   // assign IO_RAMOutput = ramOut[address];
 
 endmodule // dataOutput
